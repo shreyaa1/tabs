286fa46be3c3d06d18b8f3c2b0f0fea7b02104a2
shreyaa1 <shuklashreya801@gmail.com>
2020-12-01 20:26:52 +0530

    refactor tab  component component

--- src/topNav/index.js
+++ src/topNav/index.js
@@ -3,26 +3,26 @@ import React, { useState } from "react";
 
 export const MainTab = () => {
 
-const list = () => {
-    const tabs = [
-        {
-            name: 'Home',
+    const list = () => {
+        const tabs = [
+            {
+                name: 'Home',
 
-            component: <div>home comp</div>
-        },
-        {
-            name: 'News',
+                component: <div>home comp</div>
+            },
+            {
+                name: 'News',
 
-            component: <div>news</div>
-        },
-        {
-            name: 'About',
+                component: <div>news</div>
+            },
+            {
+                name: 'About',
 
-            component: <div>about</div>
-        },
-    ]
-    return tabs
-}
+                component: <div>about</div>
+            },
+        ]
+        return tabs
+    }
 
     const [selectedItem, handleSelectedItem] = useState(0)
 
@@ -34,15 +34,25 @@ const list = () => {
         <ul>
             {list().map((tab, index) => {
                 return <>
-                    <li key={tab}  onClick={() => renderChild(index)}>
-                    {tab.name}
-                </li>
-                    <>{selectedItem === index ? <>{tab.component}</>: null}</>
+                    <li key={tab} onClick={() => renderChild(index)}>
+                        {tab.name}
+                    </li>
                 </>
             }
             )}
-
         </ul>
+
+        <div>
+
+            {list().map((tab, index) => {
+                return <>
+                    {selectedItem === index ? <> {tab.component}</>
+                        : null}
+                </>
+            }
+            )}
+        </div>
+
 
     </div>
 } 
