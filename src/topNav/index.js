552e7aacf30991739d3312e991c8f540fd9e41c9
shreyaa1 <shuklashreya801@gmail.com>
2020-12-01 20:39:40 +0530

    added style

--- src/topNav/index.js
+++ src/topNav/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+import "./topNav.css";
 
 export const MainTab = () => {
 
@@ -7,17 +8,38 @@ export const MainTab = () => {
         const tabs = [
             {
                 name: 'Home',
-
                 component: <div>home comp</div>
             },
             {
                 name: 'News',
-
                 component: <div>news</div>
             },
             {
                 name: 'About',
-
+                component: <div>about</div>
+            },
+            {
+                name: 'tab 1',
+                component: <div>home comp</div>
+            },
+            {
+                name: 'tab 2',
+                component: <div>news</div>
+            },
+            {
+                name: 'tab 3',
+                component: <div>about</div>
+            },
+            {
+                name: 'tab 4',
+                component: <div>home comp</div>
+            },
+            {
+                name: 'tab 5',
+                component: <div>news</div>
+            },
+            {
+                name: 'tab 6',
                 component: <div>about</div>
             },
         ]
@@ -30,8 +52,8 @@ export const MainTab = () => {
         handleSelectedItem(index)
     }
 
-    return <div>
-        <ul>
+    return <div className='wrapper'>
+        <ul className='tab-list'>
             {list().map((tab, index) => {
                 return <>
                     <li key={tab} onClick={() => renderChild(index)}>
