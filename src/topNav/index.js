a78eb47b11adf5d433c8b04a22ff73baf3e3fd95
shreyaa1 <shuklashreya801@gmail.com>
2020-12-01 19:23:25 +0530

    added basic tab and gitignore file

--- src/topNav/index.js
+++ src/topNav/index.js
@@ -1,16 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 
-export const NavBar = ({ tabs, children, selectedTab }) => {
+export const MainTab = () => {
+
+const list = () => {
+    const tabs = [
+        {
+            name: 'Home',
+
+            component: <div>home comp</div>
+        },
+        {
+            name: 'News',
+
+            component: <div>news</div>
+        },
+        {
+            name: 'About',
+
+            component: <div>about</div>
+        },
+    ]
+    return tabs
+}
+
+    const [selectedItem, handleSelectedItem] = useState(0)
+
+    const renderChild = (index) => {
+        handleSelectedItem(index)
+    }
+
     return <div>
         <ul>
-            {tabs.map(tab => {
-                <li onClick={selectedTab()}>
-                    {tab}
+            {list().map((tab, index) => {
+                return <>
+                    <li key={tab}  onClick={() => renderChild(index)}>
+                    {tab.name}
                 </li>
+                    <>{selectedItem === index ? <>{tab.component}</>: null}</>
+                </>
             }
             )}
+
         </ul>
-        {children}
+
     </div>
-}
+} 
